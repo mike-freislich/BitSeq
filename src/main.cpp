6e41cecab2f139b7c97f73b8ae9d0c2c2ff44602
Mike Freislich <mike.freislich@gmail.com>
2021-12-02 11:27:25 +0200

    fixed chars

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 #include <ShiftRegister74HC595.h>
+#include <pins_arduino.h>
+
 
 const int rs = 1, en = 4, d4 = 5, d5 = 6, d6 = 7, d7 = 3;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
@@ -8,8 +10,8 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 uint8_t step = 0;
 uint8_t x = 0, y = 0;
 uint32_t last = 0;
-uint8_t bpm = 120;
-uint8_t gatePercent = 80;
+uint8_t bpm = 130;
+uint8_t gatePercent = 90;
 uint16_t tempoMS = 60.0 / bpm / 4.0 * 1000;
 
 #define trackLength 64
@@ -43,18 +45,37 @@ void setup()
   track[14] = 0b0100010001000100000000000000000000000000000000000000000000000000; // o-hh
   track[15] = 0b1011101110111011000000000000000000000000000000000000000000000000; // c-hh
   
-  //sr.setAllLow();
+  //sr.setAllLow(); 
+  byte c1[8] = {0b00000,0b00000,0b00000,0b00000,0b00000,0b00000,0b00000,0b11111};    
+  byte c2[8] = {0b00000,0b00000,0b00000,0b00000,0b00000,0b00000,0b11111,0b11111};
+  byte c3[8] = {0b00000,0b00000,0b00000,0b00000,0b00000,0b11111,0b11111,0b11111};
+  byte c4[8] = {0b00000,0b00000,0b00000,0b00000,0b11111,0b11111,0b11111,0b11111};
+  byte c5[8] = {0b00000,0b00000,0b00000,0b11111,0b11111,0b11111,0b11111,0b11111};
+  byte c6[8] = {0b00000,0b00000,0b11111,0b11111,0b11111,0b11111,0b11111,0b11111};
+  byte c7[8] = {0b00000,0b11111,0b11111,0b11111,0b11111,0b11111,0b11111,0b11111};
+  byte c8[8] = {0b11111,0b11111,0b11111,0b11111,0b11111,0b11111,0b11111,0b11111};
+  
   lcd.begin(16, 2);  
-  setGate(gatePercent);
+  
+  lcd.createChar(0, c1);
+  lcd.createChar(1, c2);
+  lcd.createChar(2, c3);
+  lcd.createChar(3, c4);
+  lcd.createChar(4, c5);
+  lcd.createChar(5, c6);
+  lcd.createChar(6, c7);
+  lcd.createChar(7, c8);
+  lcd.noCursor();
+  //delay(10);
+  //setGate(90);
 }
 
 bool cleared;
 
 void loop()
-{
-
+{  
   if (step % 16 == 0) {
-    setGate(random(10,100));
+    setGate(random(10,100));    
   }
 
   uint32_t now = millis();
@@ -62,7 +83,6 @@ void loop()
 
   if (!cleared && (elapsed >= tempoMS * gatePercent/100.0)) {
     //sr.setAllLow();
-    //lcd.clear();
     cleared = true;
   }
 
@@ -79,17 +99,9 @@ void loop()
 
 void setGate(uint8_t percent) {
 
+  if (gatePercent == percent) return;
   gatePercent = percent;
-  uint8_t glyph[8];
-  uint8_t lines = 8.0 * percent / 100.0;
-  for (uint8_t i = 0; i < 8; i ++) {
-    if (i <= lines) {
-      glyph[7-i] = 0b11111;
-    } else {
-      glyph[7-i] = 0b00000;
-    }
-  }
-  lcd.createChar(0, glyph);
+ 
 }
 
 void updateDisplay()
@@ -100,6 +112,7 @@ void updateDisplay()
   
   lcd.clear();
   lcd.setCursor(0, 0);
+  lcd.noCursor();
   lcd.print(String(bar) + "-" + String(quarter) + "-" + String(sixteenth));
   lcd.setCursor(9,0);
   lcd.print("bpm:" + String(bpm));
@@ -110,8 +123,8 @@ void updateDisplay()
   {
     if (bitRead(track[i], step)) {
       bitSet(stepData, i);
-      lcd.setCursor(i, 1);
-      lcd.write(byte(0));
+      lcd.setCursor(i, 1);            
+      lcd.write(byte((byte) (8.0 * (int)gatePercent / (float)100)));
     }  
     
   }
